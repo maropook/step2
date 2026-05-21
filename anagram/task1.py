77eab751b882f781128c90c4ff7ad9b84a4848f2
hasegawa_itsuki <ii440092@icloud.com>
2026-05-21 21:05:51 +0900

    finish task1-2

--- anagram/task1.py
+++ anagram/task1.py
@@ -1,110 +1,97 @@
 import sys
 from collections import defaultdict
 
-# WORDS_FILE = "words.txt"
-WORDS_FILE = "test_words.txt"
+"""
+usage python3 task1.py <query>
+query should be string, len(string) > 0
+"""
+WORDS_FILE = "words.txt"
 
 
-def main(data_file):
-    query_words = read_words(data_file)
+def main(query):
     dictionary = create_sorted_dictionary(read_words(WORDS_FILE))
-
-    ans_dict = defaultdict(list)
-    if not query_words:
-        print("no query found")
-        return
-    for query in query_words:
-        anagrams = get_anagram(query, dictionary)
-        ans_dict[query] = anagrams
-
-    print(ans_dict.items())
+    answer_list = get_anagram(query, dictionary)
+    print(answer_list)
 
 
-def create_sorted_dictionary(dictionary):
-    new_dictionary = []
-    for dict_word in dictionary:
-        new_dictionary.append((sorted(dict_word), dict_word))
-    sorted_new_dictionary = sorted(new_dictionary)
-    # print(f"dictionary:{sorted_new_dictionary}")
-    return sorted_new_dictionary
+def get_anagram_with_base_dictionary(query):
+    dictionary = create_sorted_dictionary(read_words(WORDS_FILE))
+    return get_anagram(query, dictionary)
 
 
-def get_anagram(random_word, dictionary):
-    N = len(dictionary)
-    ans = []
-    sorted_random_word = sorted(random_word)
-    answer_index = binary_search_word(sorted_random_word, dictionary)
-    print(f"answer_index:{answer_index}")
-    ans = select_answers(sorted_random_word, answer_index, dictionary)
-    return ans
+def create_sorted_dictionary(raw_dictionary):
+    dictionary = []
+    for word in raw_dictionary:
+        dictionary.append((sorted(word), word))
+    return sorted(dictionary)
 
 
-def select_answers(word, answer_index, dictionary):
+def get_anagram(raw_query, dictionary):
+    sorted_query = sorted(raw_query)
+    answer_index = binary_search_word(sorted_query, dictionary)
+    return get_answer_from_dictionary(sorted_query, answer_index, dictionary)
+
+
+def binary_search_word(query, dictionary):
+    # 文字を2文探索でやる
+    # 答えを見つけたらtwo pointersをしてもいいかもしれない
+    # 文字の先頭から見ていって、マッチ度で見ていく
+    # 1つでも一致しない場合はmidを更新
+    # 文字が一致する間はwhileで進める、一致しなくなったらqueryが小さいか大きいかで分岐
+    # len(query), len(dict) wordの小さい方でloopする
+    # returnされなかった場合は
+    # 一致してたけどどちらかの文字が足りない
+    # query文字が長い場合→left = mid +1
+    # query文字が短い場合→right = mid -1
+    # 一致でloop抜けたら文字数が同じ、違う、で分岐する
+    if not query or not dictionary:
+        return -1
+    left, right = 0, len(dictionary) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        dict_word = dictionary[mid][0]
+        common_length = min(len(query), len(dict_word))
+        for i in range(common_length):
+            # print(f"query:{query}, dict_word:{dict_word}")
+            # print(
+            #     f"left:{left} mid:{mid} right:{right} index:{i} query:{query[i]} dict:{dict_word[i]} query:{query} dict:{dict_word}"
+            # )
+            if ord(dict_word[i]) > ord(query[i]):
+                right = mid - 1
+                break
+            elif ord(dict_word[i]) < ord(query[i]):
+                left = mid + 1
+                break
+            else:
+                # 現在のcharが一致した場合
+                is_last_common_loop = common_length == i + 1
+                if is_last_common_loop:  # 共通した長さまでMAX一致した場合
+                    if len(query) == len(dict_word):
+                        return mid
+                    elif len(query) > len(dict_word):
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+    return -1
+
+
+def get_answer_from_dictionary(word, answer_index, dictionary):
     ans = []
     if answer_index == -1:
         return ans
-    print(answer_index)
     # 答えを見つけたものの、他にも選択肢があるかもしれない
     # dictionaryのindexが一番小さいanagramを探す
-    N = len(dictionary)
     current_index = answer_index - 1
     while 0 <= current_index and dictionary[current_index][0] == word:
         ans.append(dictionary[current_index][1])
         current_index -= 1
 
     current_index = answer_index
-    while current_index < N and dictionary[current_index][0] == word:
+    while current_index < len(dictionary) and dictionary[current_index][0] == word:
         ans.append(dictionary[current_index][1])
         current_index += 1
-    print(ans)
     return ans
-
-
-def binary_search_word(word, dictionary):
-    # 文字を2文探索でやる
-    # 答えを見つけたらtwo pointersをしてもいいかもしれない
-    # 文字の先頭から見ていって、マッチ度で見ていく
-    # 1つでも一致しない場合はmidを更新
-    # 文字が一致する間はwhileで進める、一致しなくなったらqueryが小さいか大きいかで分岐
-    # len(word), len(dict) wordの小さい方でloopする
-    # returnされなかった場合は
-    # 一致してたけどどちらかの文字が足りない
-    # query文字が長い場合→left = mid +1
-    # query文字が短い場合→right = mid -1
-    # 一致でloop抜けたら文字数が同じ、違う、で分岐する
-    left, right = 0, len(dictionary)
-
-    while left <= right:
-        mid = (left + right) // 2
-        dict_word = dictionary[mid][0]
-        common_len = min(len(word), len(dict_word))
-
-        for i in range(common_len):
-            # print(f"word:{word}, dict_word:{dict_word}")
-            # print(
-            #     f"left:{left} mid:{mid} right:{right} index:{i} word:{word[i]} dict:{dict_word[i]} query:{word} dict:{dict_word}"
-            # )
-            char_dict = dict_word[i]
-            char_query = word[i]
-
-            if char_dict == char_query:
-                if common_len == i + 1:
-                    # 一致でloop抜けたら文字数が同じ、違う、で分岐する
-                    if len(word) == len(dict_word):
-                        print("middle found")
-                        return mid
-                    elif len(word) > len(dict_word):
-                        left = mid + 1
-                    else:
-                        right = mid - 1
-            elif ord(char_dict) > ord(char_query):
-                right = mid - 1
-                break
-            else:
-                left = mid + 1
-                break
-    # print(f"current_target  left:{left} mid:{mid} right:{right} ")
-    return -1
 
 
 def read_words(word_file):
@@ -127,4 +114,3 @@ if __name__ == "__main__":
         print("usage: %s query_file output_file" % sys.argv[0])
         exit(1)
     main(sys.argv[1])
-    # main(sys.argv[1], sys.argv[2])
---
+++ anagram/task2.py
@@ -0,0 +1,89 @@
+import sys
+from collections import defaultdict
+from score_checker import calculate_score
+
+WORDS_FILE = "words.txt"
+# WORDS_FILE = "test_words.txt"
+
+# 与えられた文字列をすべて使わなくて良い→queryと辞書をalphabetそれぞれいくつ文字使っているかを記憶する
+# query > dictionaryでよい タモリ > モリ
+# query include dictionary
+# query - dictionaryをして、queryのどのindexもマイナスになっていなければ良い
+# dictionaryを2次元配列に変換する a,b,c,d,e,f,gとかが入る配列にして、上から順番に見ていって追記していくしかないのでは
+# コピーしまくるのは良くなさそう、
+# words_listを2次元配列にする len(dict) * 26
+# word_counted_dictionary = [[0]*26 for _ in range(len(dictionary))]
+
+# 1つ1つ見ていってだめならreturnするってだけで良さそう
+
+# dictionaryの中身をスコアの高いにosrtして当てはまったら即時returnする
+
+ALPHABET_COUNT = 26
+
+
+def create_word_aphabet_count(word):
+    alphabet_counts = [0] * ALPHABET_COUNT
+    for c in word:
+        alphabet_counts[ord(c) - ord("a")] += 1
+    return alphabet_counts
+
+
+def create_dictionary_alphabet_count(words):
+    alphabet_counts_dict = defaultdict(list)
+    for word in words:
+        alphabet_counts = [0] * ALPHABET_COUNT
+        for c in word:
+            alphabet_counts[ord(c) - ord("a")] += 1
+        alphabet_counts_dict[word] = alphabet_counts
+    return alphabet_counts_dict
+
+
+def main(data_file):
+
+    dictionary = read_words(WORDS_FILE)
+    sorted_dictionary = sorted(
+        dictionary, key=lambda x: calculate_score(x), reverse=True
+    )
+    dictionary_alphabet_counts = create_dictionary_alphabet_count(sorted_dictionary)
+    queries = read_words(data_file)
+    ans = 0
+    for query in queries:
+        new_anagram = get_new_anagram(
+            create_word_aphabet_count(query), dictionary_alphabet_counts
+        )
+        score = calculate_score(new_anagram)
+        ans += score
+        print(f"{query}: new_anagram:{new_anagram}, score:{score}")
+    print(ans)
+
+
+def get_new_anagram(query, dictonary):
+    for dict_word, alphabet_count in dictonary.items():
+        for i in range(ALPHABET_COUNT):
+            if query[i] < alphabet_count[i]:
+                break
+            if i == ALPHABET_COUNT - 1:
+                return dict_word
+    return ""
+
+
+def read_words(word_file):
+    words = []
+    with open(word_file) as f:
+        for line in f:
+            words.append(line.strip("\n"))
+    return words
+
+
+def write_words(word_file):
+    with open(word_file) as f:
+        for line in f:
+            print(line, file=f)
+    return
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: %s query_file output_file" % sys.argv[0])
+        exit(1)
+    main(sys.argv[1])
